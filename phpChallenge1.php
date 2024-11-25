a468eb33d6f076f4dd4a6c5cd85d6ae8157facc9
YCRita <44810958+YCRita@users.noreply.github.com>
2024-11-25 22:22:28 +0000

    first

---
+++ phpChallenge1.php
@@ -0,0 +1,7 @@
+<?php
+    $width = 10; 
+    $height = 5;
+    $area=$width*$height;
+    
+    echo "The rectangle has a width of ".$width." meters, a height of  ".$height." meters, and an area of ".$area." square meters." ;
+ ?>
