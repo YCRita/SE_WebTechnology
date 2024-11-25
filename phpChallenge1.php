657f42cb2641a5df39a3628c0249c91a5f857e03
YCRita <44810958+YCRita@users.noreply.github.com>
2024-11-25 22:29:14 +0000

    PHP Challenge 1 - Q2 added

--- phpChallenge1.php
+++ phpChallenge1.php
@@ -1,3 +1,5 @@
+Q1.
+
 <?php
     $width = 10; 
     $height = 5;
@@ -5,3 +7,22 @@
     
     echo "The rectangle has a width of ".$width." meters, a height of  ".$height." meters, and an area of ".$area." square meters." ;
  ?>
+
+
+Q2.
+
+<?php
+
+$n1 = 10;
+$n2 = 5;
+
+echo "Addition of ".$n1." and ".$n2. " is:  ".$n1+$n2."<br>";
+
+echo "Subtraction of ".$n1." and ".$n2. " is:  ".$n1-$n2."<br>";
+
+echo "Multiplication of ".$n1." and ".$n2. " is:  ".$n1*$n2."<br>";
+
+echo "Division of ".$n1." and ".$n2. " is:  ".$n1/$n2."<br>";
+
+echo "Concatenation of ".$n1." and ".$n2. " is:  ".$n1.$n2;
+?>
