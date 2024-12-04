ab5991093965c0293fdce5213016a9fe9b9f12f6
YCRita <44810958+YCRita@users.noreply.github.com>
2024-12-04 16:17:03 +0000

    revise w1

---
+++ w1q2.php
@@ -0,0 +1,22 @@
+<?php
+$number1 = 10;
+$number2 = 5;
+
+
+
+$add = $number1 + $number2;
+echo "Addition of $number1 and $number2 is : $add<br>";
+
+$sub = $number1 - $number2;
+echo "Subtraction of $number1 and $number2 is $sub<br>";
+
+$multiple = $number1 * $number2;
+echo "Multiplication of $number1 and $number2 is $multiple<br>";
+
+$div = $number1 / $number2;
+echo "Division of $number1 and $number2 is $div<br>";
+
+$stringadd = "$number1"."$number2";
+echo "Concatenation of $number1 and $number2 is $stringadd<br>";
+
+?>
---
+++ w1q3.php
@@ -0,0 +1,18 @@
+ 
+<?php
+
+$age = 25;
+$days = $age * 365;
+$hours = $days * 24;
+$min = $hours * 60;
+
+echo "<h1>Welocme to the Age Calculator</h1>
+	<h2>Your age: $age </h2>
+	<h2> You have been alive for: </h2> 
+	  <ul> 
+	    <li>$days days</li>
+	    <li>$hours hours</li>
+	    <li>$min minutes</li>
+          <ul> ";
+	
+?>
